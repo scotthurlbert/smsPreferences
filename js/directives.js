c56541ff447145564ff078487ca69c0f9bb3a15f
Scott Hurlbert <scott.hurlbert@akqa.com>
2013-08-01 12:26:50 -0700

    Initial version of the repository.

---
+++ js/directives.js
@@ -0,0 +1,70 @@
+﻿'use strict';
+
+smsPreferencesApp.directive( 'sendSmsConfirmation', function ()
+{
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/send-sms-confirmation.html'
+    };
+} );
+
+smsPreferencesApp.directive( 'phoneNumber', function ()
+{
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/phone-number.html'
+    };
+} );
+
+smsPreferencesApp.directive( 'confirmationCodeEntry', function ()
+{
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/confirmation-code-entry.html'
+    };
+} );
+
+smsPreferencesApp.directive( 'box', function ()
+{
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/box.html'
+    };
+} );
+
+smsPreferencesApp.directive( 'checkbox', function ()
+{
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/checkbox.html'
+    };
+} );
+
+smsPreferencesApp.directive( 'timesOfDayCheckboxes', function ()
+{
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/times-of-day-checkboxes.html'
+    };
+} );
+smsPreferencesApp.directive( 'timesOfDayCheckboxes', function ()
+{
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/times-of-day-checkboxes.html'
+    };
+} );
+smsPreferencesApp.directive( 'daysOfWeekCheckboxes', function ()
+{
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/days-of-week-checkboxes.html'
+    };
+} );
+smsPreferencesApp.directive( 'saveCancelButtons', function ()
+{
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/save-cancel-buttons.html'
+    };
+} );
